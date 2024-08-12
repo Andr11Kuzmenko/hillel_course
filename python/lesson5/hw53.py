ca6f0cf543cb83e2f7df9d28f4aadebac3e54d92
AKuzmenko <a.kuzmenko.12.94@gmail.com>
2024-08-12 19:38:59 +0200

    lesson5

--- python/lesson5/hw53.py
+++ python/lesson5/hw53.py
@@ -3,7 +3,6 @@ import string
 init_str = input()
 
 cpy_str = ''.join([n for n in init_str if n not in string.punctuation])
-cpy_str = f'#{cpy_str.title().replace(' ', '')}'
-cpy_str = cpy_str if len(cpy_str) <= 140 else cpy_str[:139]
+cpy_str = f'#{cpy_str.title().replace(' ', '')}'[:139]
 
 print(cpy_str)

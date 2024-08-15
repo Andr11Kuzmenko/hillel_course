e44c86c0520d74008ccba997c302f9b37f51f485
AKuzmenko <a.kuzmenko.12.94@gmail.com>
2024-08-15 09:31:10 +0200

    lesson5

--- python/lesson5/hw51.py
+++ python/lesson5/hw51.py
@@ -8,7 +8,7 @@ input_str = input()
 first_symbol = input_str[0] if input_str else ''
 negative_result = keyword.iskeyword(input_str)
 negative_result = negative_result or input_str.count('_') == len(input_str) and len(input_str) != 1
-negative_result = negative_result or not input_str.islower()
+negative_result = negative_result or not input_str.islower() and input_str != '_'
 negative_result = negative_result or first_symbol.isdigit()
 
 for i in string.punctuation:
--- python/lesson5/hw53.py
+++ python/lesson5/hw53.py
@@ -2,7 +2,7 @@ import string
 
 init_str = input()
 
-cpy_str = ''.join([n for n in init_str if n not in string.punctuation])
-cpy_str = f'#{cpy_str.title().replace(' ', '')}'[:139]
+cpy_str = "".join([n for n in init_str if n not in string.punctuation])
+cpy_str = f"#{cpy_str.title().replace(' ', '')}"[:140]
 
 print(cpy_str)

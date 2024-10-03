c7b0e8b221593c704dabf1c22528942daac95344
AKuzmenko <a.kuzmenko.12.94@gmail.com>
2024-10-03 19:23:55 +0200

    homework_5

---
+++ front-end_pro/homework_5/task_5_1/index.js
@@ -0,0 +1,7 @@
+const nums = [];
+
+for (let i = 20; i <= 30; i += 0.5) {
+    nums.push(i);
+}
+
+console.log(nums.join(' '));
---
+++ front-end_pro/homework_5/task_5_3/index.js
@@ -0,0 +1,7 @@
+const userNum = parseInt(prompt('Enter your num:'));
+
+let i = 1;
+while (i * i < userNum && i <= 100) {
+    console.log(`current num: ${i}; its sqr: ${i * i}`);
+    i++;
+}

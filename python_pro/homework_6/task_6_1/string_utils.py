8533821c482837aecbcfd19f8c7f2f8cb37a9c79
AKuzmenko <a.kuzmenko.12.94@gmail.com>
2024-10-05 21:52:22 +0200

    homework_6

--- python_pro/homework_6/task_6_1/string_utils.py
+++ python_pro/homework_6/task_6_1/string_utils.py
@@ -1,6 +1,6 @@
 def to_uppercase(s: str) -> str:
     """
-    Converts all characters in the input string `s` to uppercase.
+    Converts all characters in the input string 's' to uppercase.
     :param s: The input string to be converted to uppercase.
     :return: A new string with all alphabetic characters in uppercase.
     """
@@ -9,7 +9,7 @@ def to_uppercase(s: str) -> str:
 
 def trim_spaces(s: str) -> str:
     """
-    Removes leading and trailing spaces from the input string `s`.
+    Removes leading and trailing spaces from the input string 's'.
     :param s: The input string from which leading and trailing spaces will be removed.
     :return: A new string with leading and trailing spaces removed.
     """

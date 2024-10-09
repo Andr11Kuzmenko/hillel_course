52a4babc15fe1c377b64177f90fe8b52ff666054
AKuzmenko <a.kuzmenko.12.94@gmail.com>
2024-10-09 16:27:41 +0200

    homework_7

---
+++ python_pro/homework_7/task_7_4.py
@@ -0,0 +1,37 @@
+"""task 7 4"""
+
+
+def is_even(n: int) -> bool:
+    """
+    Determines if a given integer is even.
+    :param n: The integer to check.
+    :return: Returns True if 'n' is even, otherwise False.
+    >>> is_even(5)
+    False
+    >>> is_even(10)
+    True
+    >>> is_even(0)
+    True
+    """
+    return not n % 2
+
+
+def factorial(n: int) -> int:
+    """
+    Calculates the factorial of a given non-negative integer 'n'.
+    :param n: A non-negative integer for which the factorial is to be computed.
+    :return: The factorial of the given number 'n'.
+    >>> factorial(2)
+    2
+    >>> factorial(3)
+    6
+    >>> factorial(0)
+    1
+    """
+    return n * factorial(n - 1) if n > 0 else 1
+
+
+if __name__ == '__main__':
+    import doctest
+
+    doctest.testmod()

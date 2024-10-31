a5d3dc2dc1037ff9c8cd91210bc38da985568215
AKuzmenko <a.kuzmenko.12.94@gmail.com>
2024-10-31 16:53:37 +0100

    homework_11

--- front-end_pro/homework_11/task_11_2/index.js
+++ front-end_pro/homework_11/task_11_2/index.js
@@ -1,18 +1,4 @@
-let defaultTextColor = true;
-
-function getRandomColor() {
-    let res = '#';
-
-    for (let i = 0; i < 6; i++) {
-        res = `${res}${Math.floor(Math.random() * 15).toString(16)}`;
-    }
-
-    return res;
-}
-
 function handleOnClick() {
-    const articleTag = document.getElementById('customArticle');
-    articleTag.style.color = !defaultTextColor ? '#000000' : getRandomColor();
-    defaultTextColor = !defaultTextColor;
+    document.getElementById('customArticle').classList.toggle('new-color');
 }
 

7a2918f87d0dc697c7f11e26bf9bb15f1ac9b52b
AKuzmenko <a.kuzmenko.12.94@gmail.com>
2024-11-01 20:26:51 +0100

    homework_13

--- front-end_pro/homework_13/task_13_1/index.js
+++ front-end_pro/homework_13/task_13_1/index.js
@@ -3,9 +3,8 @@ function setValidity(elem, msg = '') {
     elem.reportValidity();
 }
 
-const dialog = document.getElementById('modal');
-dialog.showModal();
-dialog.addEventListener('focusout', evt => {
+const form = document.querySelector('form');
+form.addEventListener('input', evt => {
     if (!['input', 'textarea'].includes(evt.target.tagName.toLowerCase())) {
         return;
     }
@@ -13,12 +12,12 @@ dialog.addEventListener('focusout', evt => {
     let isValid = false;
     let errorMsg = '';
     const targetVal = evt.target.value;
-    switch (evt.target.id) {
+    switch (evt.target.name) {
         case 'name':
             isValid = /^[a-zA-Z]+$/.test(targetVal);
             errorMsg = 'Invalid name';
             break;
-        case 'msg':
+        case 'message':
             isValid = /.{5,}/.test(targetVal);
             errorMsg = 'Should be at least 5 symbols';
             break;
@@ -26,33 +25,22 @@ dialog.addEventListener('focusout', evt => {
             isValid = /^[+]380[0-9]{9}$/.test(targetVal);
             errorMsg = 'Should start with \'+380\' and have 9 more digits';
             break;
-        case 'email':
+        case 'e-mail':
             isValid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(targetVal);
             errorMsg = 'Invalid email';
             break;
     }
 
-    if (!isValid) {
-        setValidity(evt.target, errorMsg);
-    }
-});
-dialog.addEventListener('input', evt => {
-    if (!['input', 'textarea'].includes(evt.target.tagName.toLowerCase())) {
-        return;
-    }
-
-    setValidity(evt.target);
+    setValidity(evt.target, !isValid ? errorMsg : '');
 });
 
-const btn = document.getElementById('sendBtn');
-btn.addEventListener('click', evt => {
-    const elems = document.getElementsByClassName('screen-elem');
-    const elementsArray = Array.from(elems);
-    const allValid = !!elementsArray.filter(item => !!item.value).length;
+form.addEventListener('submit', event => {
+    event.preventDefault();
 
-    if (allValid) {
-        const body = {};
-        elementsArray.forEach(item => body[item.id] = item.value);
-        console.log('Body', body)
-    }
-});
+    const formData = new FormData(event.target);
+    const formObj = {};
+
+    formData.forEach((value, key) => formObj[key] = value)
+
+    console.log(formObj);
+})

c738e8af3e3b23111f0f49ed8fc168cd8f7040e4
AKuzmenko <a.kuzmenko.12.94@gmail.com>
2024-11-01 23:45:16 +0100

    homework_11

--- python_pro/homework_11/task_11_2.py
+++ python_pro/homework_11/task_11_2.py
@@ -1,3 +1,9 @@
+"""
+This module asynchronously fetches content from multiple URLs using 'aiohttp' and handles
+logging for potential errors. It includes functionality to validate SSL certificates and log
+invalid URLs.
+"""
+
 import asyncio
 import logging
 import ssl
@@ -15,27 +21,41 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-async def fetch_content(url: str) -> str | None:
+async def fetch_content(url: str) -> str:
+    """
+    Asynchronously fetches the HTML content of a specified URL with SSL certificate validation.
+    :param url: The URL of the web page to fetch.
+    :return: The HTML content of the page if the request is successful; otherwise, an error message.
+    """
     ssl_context = ssl.create_default_context(cafile=certifi.where())
     conn = aiohttp.TCPConnector(ssl=ssl_context)
     async with aiohttp.ClientSession(connector=conn) as session:
-        response = await session.get(url)
-        # try:
-        #     response = await session.get(url)
-        # except InvalidUrlClientError as err:
-        #     logger.error(err)
-        #     print(err)
-        # else:
-        #     return await response.text()
-        return None
+        try:
+            response = await session.get(url)
+            return await response.text()
+        except InvalidUrlClientError:
+            msg = f"Invalid URL: {url}"
+            logger.error(msg)
+            return msg
 
 
-async def fetch_all(urls: list[str]) -> list[str | None]:
+async def fetch_all(urls: list[str]) -> list[str]:
+    """
+    Asynchronously fetches the content of multiple URLs.
+    :param urls: A list of URLs to fetch content from.
+    :return: A list of strings, where each string is the HTML content of a URL or
+            an error message if the URL is invalid.
+    """
     tasks = []
     for url in urls:
         tasks.append(fetch_content(url))
     return await asyncio.gather(*tasks)
 
-if __name__ == '__main__':
-    # asyncio.run(fetch_all(urls=["https://www.google.com", "https://www.yahoo.com"]))
-    print(asyncio.run(fetch_all(urls=[""])))
+
+if __name__ == "__main__":
+    res1, res2, res3 = asyncio.run(
+        fetch_all(
+            urls=["https://www.google.com", "https://www.yahoo.com", "tttttt.com"]
+        )
+    )
+    print(res3)

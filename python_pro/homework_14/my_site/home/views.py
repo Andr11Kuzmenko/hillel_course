4c65e23bcb2afa7928befae38bd5102c80803ad4
AKuzmenko <a.kuzmenko.12.94@gmail.com>
2024-11-04 02:44:33 +0100

    homework_14

---
+++ python_pro/homework_14/my_site/home/views.py
@@ -0,0 +1,21 @@
+from django.http import HttpResponse
+
+
+def home_view(request):
+    return HttpResponse('Welcome to the main page')
+
+
+def about_view(request):
+    return HttpResponse('About us')
+
+
+def contact_view(request):
+    return HttpResponse('Contact us')
+
+
+def post_view(request, id_):
+    return HttpResponse(f'You are looking at the post with the id: {id_}')
+
+
+def profile_view(request, username):
+    return HttpResponse(f'You are looking at the profile of {username}')

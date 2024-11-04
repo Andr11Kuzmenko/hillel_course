98204afc540ea0b5ec82fbb37423a710acd75143
AKuzmenko <a.kuzmenko.12.94@gmail.com>
2024-11-04 02:50:01 +0100

    homework_14

--- python_pro/homework_14/my_site/home/urls.py
+++ python_pro/homework_14/my_site/home/urls.py
@@ -3,9 +3,11 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home_view, name='home'),
-    path('about/', views.about_view, name='about'),
-    path('contact/', views.contact_view, name='contact'),
-    re_path(r'^post/(?P<id_>\d+)/', views.post_view, name='post_view'),
-    re_path(r'^profile/(?P<username>[a-zA-Z]+)/', views.profile_view, name='profile_view'),
+    path("home/", views.home_view, name="home"),
+    path("about/", views.about_view, name="about"),
+    path("contact/", views.contact_view, name="contact"),
+    re_path(r"^post/(?P<id_>\d+)/", views.post_view, name="post_view"),
+    re_path(
+        r"^profile/(?P<username>[a-zA-Z]+)/", views.profile_view, name="profile_view"
+    ),
 ]
--- python_pro/homework_14/my_site/home/views.py
+++ python_pro/homework_14/my_site/home/views.py
@@ -2,20 +2,20 @@ from django.http import HttpResponse
 
 
 def home_view(request):
-    return HttpResponse('Welcome to the main page')
+    return HttpResponse("Welcome to the main page")
 
 
 def about_view(request):
-    return HttpResponse('About us')
+    return HttpResponse("About us")
 
 
 def contact_view(request):
-    return HttpResponse('Contact us')
+    return HttpResponse("Contact us")
 
 
 def post_view(request, id_):
-    return HttpResponse(f'You are looking at the post with the id: {id_}')
+    return HttpResponse(f"You are looking at the post with the id: {id_}")
 
 
 def profile_view(request, username):
-    return HttpResponse(f'You are looking at the profile of {username}')
+    return HttpResponse(f"You are looking at the profile of {username}")

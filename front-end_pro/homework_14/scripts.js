9ecc7f571f43226380fe3d08cc7c0be67c5ce266
AKuzmenko <a.kuzmenko.12.94@gmail.com>
2024-11-10 20:55:17 +0100

    homework_14

--- front-end_pro/homework_14/scripts.js
+++ front-end_pro/homework_14/scripts.js
@@ -1,14 +1,13 @@
 const buttons = document.getElementsByTagName('button');
 Array.from(buttons).forEach(item => {
     item.addEventListener('click', event => {
-        const activeDots = document.getElementsByClassName('active-dot');
-        const firstActiveDot = activeDots[0];
-        firstActiveDot.classList.toggle('active-dot');
+        const activeDot = document.querySelector('.active-dot');
+        activeDot.classList.toggle('active-dot');
 
         if (event.target.classList.contains('prev')) {
-            firstActiveDot.previousElementSibling.classList.toggle('active-dot');
+            activeDot.previousElementSibling.classList.toggle('active-dot');
         } else {
-            firstActiveDot.nextElementSibling.classList.toggle('active-dot');
+            activeDot.nextElementSibling.classList.toggle('active-dot');
         }
 
         changeSlideByDotPosition();
@@ -22,23 +21,23 @@ Array.from(dots).forEach(item => {
             return;
         }
 
-        const activeDots = document.getElementsByClassName('active-dot');
-        Array.from(activeDots).forEach(dot => dot.classList.toggle('active-dot'));
+        const activeDot = document.querySelector('.active-dot');
+        activeDot.classList.toggle('active-dot');
         item.classList.toggle('active-dot');
         changeSlideByDotPosition();
     });
 });
 
 function changeSlideByDotPosition() {
-    const activeSlides = document.getElementsByClassName('active-slide');
-    Array.from(activeSlides).forEach(slide => slide.classList.toggle('active-slide'));
+    const activeSlide = document.querySelector('.active-slide');
+    activeSlide.classList.toggle('active-slide');
     const slidePosition = Array.from(dots).findIndex(dot => dot.classList.contains('active-dot'));
     document.getElementsByClassName('slide')[slidePosition].classList.toggle('active-slide');
     adjustButtons();
 }
 
 function adjustButtons() {
-    const activeSlide = document.getElementsByClassName('active-slide')[0];
+    const activeSlide = document.querySelector('.active-slide');
     const isThereNextSlide = !!activeSlide.nextElementSibling;
     const isTherePrevSlide = !!activeSlide.previousElementSibling;
     Array.from(buttons).forEach(btn => {
